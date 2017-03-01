c786bbce150b7267e0f29be6226aac9a7ed44e0b
Ethan <lehman@aclanyc.com>
2017-03-01 13:39:55 +0000

    my house

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-TinyTurtle.apply(window);
+function setup() {
+globalcreateCanvas(400,400);
+}
 
-// Type your directions to Tiny Turtle below
 
-
-
-stamp();
+function draw(){
+    background(51);
+    translate width(width  /2, height /2);
+    
+    beginShape();
+    for (var a = 0; a = TWO_PI; a+= 0.01)
+ var r=100; 
+ var x= 200 * cos(7*a);
+ var y= r * sin(a);
+ stroke(255);
+ stroke weight(1);
+ vertex (x, y);
+ 
+ 
+ 
+ 
+ 
+ 
+}
+endShape(CLOSE);
---
+++ script2.js
@@ -0,0 +1,17 @@
+TinyTurtle.apply(window);
+
+forward(50);
+right(45);
+forward(30);
+right(90);
+forward(30);
+right(45);
+right(90);
+forward(43);
+left(90);
+forward(50);
+left(90);
+forward(43);
+left(90);
+forward(50);
+stamp();
